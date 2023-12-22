a51a7fa7ef3a4b7e0f22fb1eff0e8b81514585dd
Johannes Mogashoa <johannes.mogashoa@dpworld.com>
2023-12-22 11:50:06 +0200

    Project Init + Init Dependencies + Navbar

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from "react";
+
+import ThemeSwitcher from "@/components/ThemeSwitcher";
+import { UserButton } from "@clerk/nextjs";
+import Logo from "@/components/Logo";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+	return (
+		<div className="flex flex-col max-h-screen min-w-full min-h-screen bg-background">
+			<nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2">
+				<Logo />
+				<div className="flex items-center gap-4">
+					<ThemeSwitcher />
+					<UserButton afterSignOutUrl="/sign-in" />
+				</div>
+			</nav>
+			<main>{children}</main>
+		</div>
+	);
+};
+
+export default Layout;
