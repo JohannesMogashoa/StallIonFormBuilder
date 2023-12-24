435ff51a87b7f95c89e593cc40581f333e8c120d
Johannes Mogashoa <johannes.mogashoa@dpworld.com>
2023-12-24 10:13:25 +0200

    02h

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -1,16 +1,24 @@
 "use client";
 
-import React, { useState } from "react";
-import DesignerSidebar from "./DesignerSidebar";
-import { DragEndEvent, useDndMonitor, useDroppable } from "@dnd-kit/core";
-import { cn } from "@/lib/utils";
+import {
+	DragEndEvent,
+	useDndMonitor,
+	useDraggable,
+	useDroppable,
+} from "@dnd-kit/core";
 import {
 	ElementsType,
 	FormElementInstance,
 	FormElements,
 } from "./FormElements";
-import useDesigner from "./hooks/useDesigner";
+import React, { useState } from "react";
+
+import { BiSolidTrash } from "react-icons/bi";
+import { Button } from "./ui/button";
+import DesignerSidebar from "./DesignerSidebar";
+import { cn } from "@/lib/utils";
 import { idGenerator } from "@/lib/idGenerator";
+import useDesigner from "./hooks/useDesigner";
 
 const Designer = () => {
 	const { elements, addElement } = useDesigner();
@@ -86,9 +94,93 @@ const DesignerElementWrapper = ({
 }: {
 	element: FormElementInstance;
 }) => {
+	const { removeElement } = useDesigner();
+
+	const [mouseIsOver, setMouseIsOver] = useState<boolean>(false);
+
+	const topHalf = useDroppable({
+		id: element.id + "-top",
+		data: {
+			type: element.type,
+			elementId: element.id,
+			isTopHalfDesignerElement: true,
+		},
+	});
+	const bottomHalf = useDroppable({
+		id: element.id + "-bottom",
+		data: {
+			type: element.type,
+			elementId: element.id,
+			isBottomHalfDesignerElement: true,
+		},
+	});
+
 	const DesignerElement = FormElements[element.type].designerComponent;
 
-	return <DesignerElement elementInstance={element} />;
+	const draggable = useDraggable({
+		id: element.id + "-drag-handler",
+		data: {
+			type: element.type,
+			elementId: element.id,
+			isDesignerElement: true,
+		},
+	});
+
+	if (draggable.isDragging) return null;
+
+	return (
+		<div
+			ref={draggable.setNodeRef}
+			{...draggable.listeners}
+			{...draggable.attributes}
+			className="relative h-[120px] flex flex-col text-foreground hover:cursor-pointer rounded-md ring-1 ring-accent ring-inset"
+			onMouseEnter={() => setMouseIsOver(true)}
+			onMouseLeave={() => setMouseIsOver(false)}
+		>
+			<div
+				ref={topHalf.setNodeRef}
+				className="absolute w-full h-1/2 rounded-t-md"
+			></div>
+			<div
+				ref={bottomHalf.setNodeRef}
+				className="absolute bottom-0 w-full h-1/2 rounded-b-md"
+			></div>
+			{mouseIsOver && (
+				<>
+					<div className="absolute right-0 h-full">
+						<Button
+							className="flex justify-center h-full bg-red-500 border rounded-md rounded-l-none"
+							variant={"outline"}
+							onClick={() => {
+								removeElement(element.id);
+							}}
+						>
+							<BiSolidTrash className="w-6 h-6" />
+						</Button>
+					</div>
+					<div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 animate-pulse">
+						<p className="text-sm text-muted-foreground">
+							Click for properties or drag to move
+						</p>
+					</div>
+				</>
+			)}
+			{topHalf.isOver && (
+				<div className="absolute top-0 w-full rounded-md rounded-b-none h-[7px] bg-primary" />
+			)}
+			{bottomHalf.isOver && (
+				<div className="absolute bottom-0 w-full rounded-md rounded-t-none h-[7px] bg-primary" />
+			)}
+			<div
+				className={cn(
+					"flex w-full h-[120px] items-center rounded-md bg-accent/40 px-4 py-2 pointer-events-none opacity-100",
+					mouseIsOver && "opacity-10"
+				)}
+			>
+				<DesignerElement elementInstance={element} />
+			</div>
+		</div>
+	);
 };
 
 export default Designer;
--- components/DragOverlayWrapper.tsx
+++ components/DragOverlayWrapper.tsx
@@ -3,9 +3,13 @@ import { ElementsType, FormElements } from "./FormElements";
 import React, { useState } from "react";
 
 import { SidebarBtnElementDragOverlay } from "./SidebarBtnElement";
+import useDesigner from "./hooks/useDesigner";
 
 const DragOverlayWrapper = () => {
+	const { elements } = useDesigner();
+
 	const [draggedItem, setDraggedItem] = useState<Active | null>(null);
+
 	useDndMonitor({
 		onDragStart: (event) => {
 			setDraggedItem(event.active);
@@ -27,6 +31,25 @@ const DragOverlayWrapper = () => {
 		node = (
 			<SidebarBtnElementDragOverlay formElement={FormElements[type]} />
 		);
+	}
+
+	const isDesignerElement = draggedItem?.data?.current?.isDesignerElement;
+
+	if (isDesignerElement) {
+		const elementId = draggedItem.data?.current?.elementId;
+		const element = elements.find((el) => el.id === elementId);
+
+		if (!element) node = <div>Element not found!!!</div>;
+		else {
+			const DesignerElementComponent =
+				FormElements[element.type].designerComponent;
+
+			node = (
+				<div className="flex bg-accent border rounded-md h-[120px] w-full py-2 px-4 opacity-80">
+					<DesignerElementComponent elementInstance={element} />
+				</div>
+			);
+		}
 	}
 
 	return <DragOverlay>{node}</DragOverlay>;
